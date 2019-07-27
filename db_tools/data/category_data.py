ae40f0a4a639d411ee3ef681f457a2b2bd7c706d
huanshenyi <txy1226052@gmail.com>
2019-07-27 20:11:48 +0900

    テストデータを制作途中

---
+++ db_tools/data/category_data.py
@@ -0,0 +1,687 @@
+row_data = [
+    {
+        'sub_categorys': [
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'yr',
+                        'name': '土用丑の日「うなぎ」'
+                    },
+                    {
+                        'code': 'ql',
+                        'name': 'セイユーカカク・超得'
+                    },
+                    {
+                        'code': 'zr',
+                        'name': '見逃せない安さ　菓子・飲料・酒'
+                    },
+                    {
+                        'code': 'nr',
+                        'name': '見逃せない安さ　食品'
+                    },
+                    {
+                        'code': '',
+                        'name': '見逃せない安さ　チルド・冷凍食品'
+                    }
+                ],
+                'code': 'bargain',
+                'name': '今月の買い得品・季節特集'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'cb',
+                        'name': '乳製品'
+                    },
+                    {
+                        'code': 'yu',
+                        'name': '冷凍食品'
+                    },
+                    {
+                        'code': 'xia',
+                        'name': 'インスタント食品'
+                    },
+                    {
+                        'code': 'xb',
+                        'name': '飲料'
+                    },
+                    {
+                        'code': 'xb',
+                        'name': '粉末飲料'
+                    },
+                    {
+                        'code': 'xb',
+                        'name': 'お酒'
+                    },
+                    {
+                        'code': 'xb',
+                        'name': '和洋菓子'
+                    },
+                    {
+                        'code': 'xb',
+                        'name': 'フレ-ク'
+                    },
+                    {
+                        'code': 'xb',
+                        'name': 'お米・麺類'
+                    },
+                    {
+                        'code': '',
+                        'name': '豆腐・納豆・漬物'
+                    },
+                    {
+                        'code': '',
+                        'name': '佃煮・惣菜'
+                    },
+                    {
+                        'code': '',
+                        'name': 'ハム・練り物'
+                    },
+                    {
+                        'code': '',
+                        'name': '缶詰・乾物・ふりかけ'
+                    }
+                ],
+                'code': 'boiled',
+                'name': '皆様のお墨付き'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'beautyCare',
+                        'name': 'ビューティケア'
+                    },
+                    {
+                        'code': 'paper',
+                        'name': 'ペーパー紙皿'
+                    },
+                    {
+                        'code': 'pet',
+                        'name': 'ペット'
+                    },
+                    {
+                        'code': 'dailyNecessities',
+                        'name': '日用品'
+                    },
+                    {
+                        'code': 'homeClean',
+                        'name': 'ホームクリーン'
+                    },
+                    {
+                        'code': 'careProducts',
+                        'name': '介護用品'
+                    },
+                    {
+                        'code': 'kitchenUtensils',
+                        'name': 'キッチン用品'
+                    },
+                    {
+                        'code': 'sweets',
+                        'name': 'お菓子'
+                    },
+                    {
+                        'code': 'beverage',
+                        'name': '飲料'
+                    },
+                    {
+                        'code': 'sake',
+                        'name': 'お酒'
+                    },
+                    {
+                        'code': 'food',
+                        'name': '食品'
+                    },
+                    {
+                        'code': 'seasoning',
+                        'name': '調味料'
+                    }
+                ],
+                'code': 'basic',
+                'name': '基本の基'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'pat',
+                        'name': 'ペット'
+                    },
+                    {
+                        'code': 'food',
+                        'name': '食品'
+                    },
+                    {
+                        'code': 'sweets',
+                        'name': '菓子'
+                    },
+                    {
+                        'code': 'beverage',
+                        'name': '飲料'
+                    }
+                ],
+                'code': 'newProduct',
+                'name': '新商品'
+            },
+            {
+                'sub_categorys': [
+                  {
+                       'code': 'importedDailyNecessities',
+                       'name': '輸入日用品'
+                  },
+                  {
+                      'code': 'importedFood',
+                      'name': '輸入食品'
+                  },
+                  {
+                      'code': 'importedSake',
+                      'name': '輸入お酒'
+                  }
+                ],
+                'code': 'importedGoods',
+                'name': '輸入品'
+            }
+        ],
+        'code': 'Recommended',
+        'name': '当店のおすすめ'
+    },
+    {
+        'sub_categorys': [
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'wly',
+                        'name': '五粮液'
+                    },
+                    {
+                        'code': 'lzlj',
+                        'name': '泸州老窖'
+                    },
+                    {
+                        'code': 'mt',
+                        'name': '茅台'
+                    }
+                ],
+                'code': 'bk',
+                'name': '白酒'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': 'ptj',
+                'name': '葡萄酒'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': 'yj',
+                'name': '洋酒'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': 'pj',
+                'name': '啤酒'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'qtpp',
+                        'name': '其他品牌'
+                    },
+                    {
+                        'code': 'hj',
+                        'name': '黄酒'
+                    },
+                    {
+                        'code': 'ysj',
+                        'name': '养生酒'
+                    }
+                ],
+                'code': 'qtjp',
+                'name': '其他酒品'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': 'yls',
+                'name': '饮料/水'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': 'bld',
+                        'name': 'アスパラガス'
+                    },
+                    {
+                        'code': 'wsj',
+                        'name': '威士忌'
+                    }
+                ],
+                'code': 'hj',
+                'name': 'いつもの野菜'
+            }
+        ],
+        'code': 'jsyl',
+        'name': '野菜・果物'
+    },
+    {
+        'sub_categorys': [
+            {
+                'sub_categorys': [
+                    {
+                        'code': '其他食用油',
+                        'name': '其他食用油'
+                    },
+                    {
+                        'code': '菜仔油',
+                        'name': '菜仔油'
+                    },
+                    {
+                        'code': '花生油',
+                        'name': '花生油'
+                    },
+                    {
+                        'code': '橄榄油',
+                        'name': '橄榄油'
+                    },
+                    {
+                        'code': '礼盒',
+                        'name': '礼盒'
+                    }
+                ],
+                'code': '食用油',
+                'name': '食用油'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': '面粉/面条',
+                        'name': '面粉/面条'
+                    },
+                    {
+                        'code': '大米',
+                        'name': '大米'
+                    },
+                    {
+                        'code': '意大利面',
+                        'name': '意大利面'
+                    }
+                ],
+                'code': '米面杂粮',
+                'name': '米面杂粮'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': '调味油/汁',
+                        'name': '调味油/汁'
+                    },
+                    {
+                        'code': '酱油/醋',
+                        'name': '酱油/醋'
+                    }
+                ],
+                'code': '厨房调料',
+                'name': '厨房调料'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '南北干货',
+                'name': '南北干货'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '方便速食',
+                'name': '方便速食'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '调味品',
+                'name': '调味品'
+            }
+        ],
+        'code': '粮油副食',
+        'name': '粮油副食'
+    },
+    {
+        'sub_categorys': [
+            {
+                'sub_categorys': [
+                    {
+                        'code': '西红柿',
+                        'name': '西红柿'
+                    },
+                    {
+                        'code': '韭菜',
+                        'name': '韭菜'
+                    },
+                    {
+                        'code': '青菜',
+                        'name': '青菜'
+                    }
+                ],
+                'code': '有机蔬菜',
+                'name': '有机蔬菜'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': '甘蓝',
+                        'name': '甘蓝'
+                    },
+                    {
+                        'code': '胡萝卜',
+                        'name': '胡萝卜'
+                    },
+                    {
+                        'code': '黄瓜',
+                        'name': '黄瓜'
+                    }
+                ],
+                'code': '精选蔬菜',
+                'name': '精选蔬菜'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': '火龙果',
+                        'name': '火龙果'
+                    },
+                    {
+                        'code': '菠萝蜜',
+                        'name': '菠萝蜜'
+                    },
+                    {
+                        'code': '奇异果',
+                        'name': '奇异果'
+                    }
+                ],
+                'code': '进口水果',
+                'name': '进口水果'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': '水果礼盒',
+                        'name': '水果礼盒'
+                    },
+                    {
+                        'code': '苹果',
+                        'name': '苹果'
+                    },
+                    {
+                        'code': '雪梨',
+                        'name': '雪梨'
+                    }
+                ],
+                'code': '国产水果',
+                'name': '国产水果'
+            }
+        ],
+        'code': '蔬菜水果',
+        'name': '蔬菜水果'
+    },
+    {
+        'sub_categorys': [
+            {
+                'sub_categorys': [
+                    {
+                        'code': '果冻',
+                        'name': '果冻'
+                    },
+                    {
+                        'code': '枣类',
+                        'name': '枣类'
+                    },
+                    {
+                        'code': '蜜饯',
+                        'name': '蜜饯'
+                    },
+                    {
+                        'code': '肉类零食',
+                        'name': '肉类零食'
+                    },
+                    {
+                        'code': '坚果炒货',
+                        'name': '坚果炒货'
+                    }
+                ],
+                'code': '休闲零食',
+                'name': '休闲零食'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': '创意喜糖',
+                        'name': '创意喜糖'
+                    },
+                    {
+                        'code': '口香糖',
+                        'name': '口香糖'
+                    },
+                    {
+                        'code': '软糖',
+                        'name': '软糖'
+                    },
+                    {
+                        'code': '棒棒糖',
+                        'name': '棒棒糖'
+                    }
+                ],
+                'code': '糖果',
+                'name': '糖果'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': '夹心巧克力',
+                        'name': '夹心巧克力'
+                    },
+                    {
+                        'code': '白巧克力',
+                        'name': '白巧克力'
+                    },
+                    {
+                        'code': '松露巧克力',
+                        'name': '松露巧克力'
+                    },
+                    {
+                        'code': '黑巧克力',
+                        'name': '黑巧克力'
+                    }
+                ],
+                'code': '巧克力',
+                'name': '巧克力'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': '牛肉干',
+                        'name': '牛肉干'
+                    },
+                    {
+                        'code': '猪肉脯',
+                        'name': '猪肉脯'
+                    },
+                    {
+                        'code': '牛肉粒',
+                        'name': '牛肉粒'
+                    },
+                    {
+                        'code': '猪肉干',
+                        'name': '猪肉干'
+                    }
+                ],
+                'code': '肉干肉脯/豆干',
+                'name': '肉干肉脯/豆干'
+            },
+            {
+                'sub_categorys': [
+                    {
+                        'code': '鱿鱼足',
+                        'name': '鱿鱼足'
+                    },
+                    {
+                        'code': '鱿鱼丝',
+                        'name': '鱿鱼丝'
+                    },
+                    {
+                        'code': '墨鱼/乌贼',
+                        'name': '墨鱼/乌贼'
+                    },
+                    {
+                        'code': '鱿鱼仔',
+                        'name': '鱿鱼仔'
+                    },
+                    {
+                        'code': '鱿鱼片',
+                        'name': '鱿鱼片'
+                    }
+                ],
+                'code': '鱿鱼丝/鱼干',
+                'name': '鱿鱼丝/鱼干'
+            }
+        ],
+        'code': '休闲食品',
+        'name': '休闲食品'
+    },
+    {
+        'sub_categorys': [
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '进口奶品',
+                'name': '进口奶品'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '国产奶品',
+                'name': '国产奶品'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '奶粉',
+                'name': '奶粉'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '有机奶',
+                'name': '有机奶'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '原料奶',
+                'name': '原料奶'
+            }
+        ],
+        'code': '奶类食品',
+        'name': '奶类食品'
+    },
+    {
+        'sub_categorys': [
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '菌菇类',
+                'name': '菌菇类'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '腌干海产',
+                'name': '腌干海产'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '汤料',
+                'name': '汤料'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '豆类',
+                'name': '豆类'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '干菜/菜干',
+                'name': '干菜/菜干'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '干果/果干',
+                'name': '干果/果干'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '豆制品',
+                'name': '豆制品'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '腊味',
+                'name': '腊味'
+            }
+        ],
+        'code': '天然干货',
+        'name': '天然干货'
+    },
+    {
+        'sub_categorys': [
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '白茶',
+                'name': '白茶'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '红茶',
+                'name': '红茶'
+            },
+            {
+                'sub_categorys': [
+
+                ],
+                'code': '绿茶',
+                'name': '绿茶'
+            }
+        ],
+        'code': '精选茗茶',
+        'name': '精选茗茶'
+    }
+]
